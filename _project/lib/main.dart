4b80364db44bd81bc0783ee2e7c384171c076569
Mykhailo Temnokhud <mtemnohud@gmail.com>
2022-12-26 05:17:17 +0100

    dev: set path strategy

--- _project/lib/main.dart
+++ _project/lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 import 'app.dart';
 import 'utils/preferences.dart';
 
 void main() async {
+  setPathUrlStrategy();
+
   Hive.initFlutter();
 
   await Preferences.initialize();
